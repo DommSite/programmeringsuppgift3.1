35dab6d7b5721175c65c9e5447ccaa0b518d225b
DommSite <johan.johansson06@live.se>
2023-09-22 12:53:15 +0200

    Add project files.

---
+++ programmeringsuppgift3.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Uppgift_3.7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Medlemsansökan till golfklubben");
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder >= 50)
+            {
+                Console.WriteLine("Välkommen till golfklubben");
+                    }
+            else
+            {
+                Console.WriteLine("Tyvärr är du för ung för att gå med i golfklubben");
+            }
+        }
+    }
+}
